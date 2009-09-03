4c87082b48390b969ad1e6d6f3509f76ca439846
Nando <fernando.garcia@the-cocktail.com>
2009-09-03 22:03:09 +0200

    Spec refactoring using macros

--- spec/models/feedsucker_feed_spec.rb
+++ spec/models/feedsucker_feed_spec.rb
@@ -1,6 +1,54 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+module FeedsuckerMacros
+  def self.included(receiver)  
+    receiver.extend         ExampleGroupMethods  
+  end
+  
+  module ExampleGroupMethods
+    def it_should_suck_all_posts_if_no_number_is_given
+      it "should suck all the posts if no number is given" do
+        @feed.suck!
+        @feed.posts.size.should == 5
+        posts = FeedsuckerFeed.find_by_title(@feed.title).posts
+        posts.size.should == 5
+        posts.first.title.should == 'Last Post Title' 
+        posts.last.title.should == 'First Post Title'
+      end
+    end
+
+    def it_should_suck_only_the_right_number_of_posts
+      it "should suck only the right number of posts" do
+        @feed.update_attribute(:number_of_posts, 2)
+        @feed.suck!
+        FeedsuckerFeed.find_by_title(@feed.title).posts.size.should == 2
+        @feed.posts.size.should == @feed.number_of_posts
+        @feed.posts.first.title.should == 'Last Post Title' 
+      end
+    end
+    
+    def it_should_suck_all_the_posts_if_we_ask_more_posts_than_the_feed_has
+      it "should suck all the posts if we ask more posts than the feed has" do
+        @feed.update_attribute(:number_of_posts, 12)
+        @feed.suck!
+        @feed.posts.size.should == 5
+      end
+    end
+
+    def it_should_not_destroy_existing_posts_with_an_empty_or_non_standard_feed
+      it "should not destroy existing posts with an empty or non standard feed" do
+        @feed.suck!
+        FakeWeb.register_uri(:get, @feed.url, :body => '<rss></rss>')
+        @feed.suck!
+        @feed.posts.size.should == 5
+      end
+    end
+  end
+end
+
 describe FeedsuckerFeed, ' with a valid RSS feed' do
+  include FeedsuckerMacros
+
   RSS_FILE_PATH = (File.dirname(__FILE__) + '/../resources/example.rss')
 
   before(:each) do
@@ -10,38 +58,15 @@ describe FeedsuckerFeed, ' with a valid RSS feed' do
     FakeWeb.register_uri(:get, @feed.url, :body => File.read(RSS_FILE_PATH))
   end
 
-  it "should suck all the posts if no number is given" do
-    @feed.suck!
-    @feed.posts.size.should == 5
-    posts = FeedsuckerFeed.find_by_title(@feed.title).posts
-    posts.size.should == 5
-    posts.first.title.should == 'Last Post Title' 
-    posts.last.title.should == 'First Post Title' 
-  end
-
-  it "should suck only the right number of posts" do
-    @feed.update_attribute(:number_of_posts, 2)
-    @feed.suck!
-    FeedsuckerFeed.find_by_title(@feed.title).posts.size.should == 2
-    @feed.posts.size.should == @feed.number_of_posts
-    @feed.posts.first.title.should == 'Last Post Title' 
-  end
-
-  it "should suck all the posts if we ask more posts than the feed has" do
-    @feed.update_attribute(:number_of_posts, 12)
-    @feed.suck!
-    @feed.posts.size.should == 5
-  end
-
-  it "should not destroy existing posts with an empty or non standard feed" do
-    @feed.suck!
-    FakeWeb.register_uri(:get, @feed.url, :body => '<rss></rss>')
-    @feed.suck!
-    @feed.posts.size.should == 5
-  end
+  it_should_suck_all_posts_if_no_number_is_given
+  it_should_suck_only_the_right_number_of_posts
+  it_should_suck_all_the_posts_if_we_ask_more_posts_than_the_feed_has
+  it_should_not_destroy_existing_posts_with_an_empty_or_non_standard_feed
 end
 
 describe FeedsuckerFeed, ' with an XML feed' do
+  include FeedsuckerMacros
+  
   XML_FILE_PATH = (File.dirname(__FILE__) + '/../resources/example.xml')
 
   before(:each) do
@@ -57,34 +82,9 @@ describe FeedsuckerFeed, ' with an XML feed' do
     FakeWeb.register_uri(:get, @feed.url, :body => File.read(XML_FILE_PATH))
   end
 
-  it "should suck all the posts if no number is given" do
-    @feed.suck!
-    posts = FeedsuckerFeed.find_by_title(@feed.title).posts
-    posts.size.should == 5
-    posts.first.blog_title.should == 'Last Blog Title' 
-    posts.last.blog_title.should == 'First Blog Title' 
-    posts.first.title.should == 'Last Post Title' 
-    posts.last.title.should == 'First Post Title' 
-  end
-
-  it "should suck only the right number of posts" do
-    @feed.update_attribute(:number_of_posts, 2)
-    @feed.suck!
-    FeedsuckerFeed.find_by_title(@feed.title).posts.size.should == 2
-    @feed.posts.size.should == @feed.number_of_posts
-    @feed.posts.first.title.should == 'Last Post Title' 
-  end
-
-  it "should suck all the posts if we ask more posts than the feed has" do
-    @feed.update_attribute(:number_of_posts, 12)
-    @feed.suck!
-    @feed.posts.size.should == 5
-  end
-
-  it "should not destroy existing posts with an empty or non standard feed" do
-    @feed.suck!
-    FakeWeb.register_uri(:get, @feed.url, :body => '<rss></rss>')
-    @feed.suck!
-    @feed.posts.size.should == 5
-  end
+  it_should_suck_all_posts_if_no_number_is_given
+  it_should_suck_only_the_right_number_of_posts
+  it_should_suck_all_the_posts_if_we_ask_more_posts_than_the_feed_has
+  it_should_not_destroy_existing_posts_with_an_empty_or_non_standard_feed
 end
+
